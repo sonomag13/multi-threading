48019b0032fa366ca1ed86ff4fe63bf74f4d6b72
Lu Gao <lgao23@apple.com>
2024-09-06 16:33:36 -0700

    additional example

--- faults/main.cpp
+++ faults/main.cpp
@@ -8,10 +8,25 @@ void func1(int & x) {
     x++;
 }
 
-int main() {
-    int x {5};
-    std::thread t(func1, std::ref(x));
+void run_func1(int x) {
 
+    std::thread t(func1, std::ref(x));
+    // t.join(); without join, run_fun1 will keep running and return; local variable x will be
+    // destroyed, meaning func1 running on thread t will write to an delocated space
     std::cout << "x = " << x << '\n';
+}
+
+int main() {
+
+    // Example 1: use std::ref to mark that x is passed by reference
+    int x1{5};
+    std::thread t(func1, std::ref(x1));
+    t.join();
+    std::cout << "x1 = " << x1 << '\n';
+
+    // Example 2:bffdsagew
+    int x2{10};
+    run_func1(x2);
+
     return 0;
 }
